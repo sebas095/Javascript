1a4d2603451340d6d03cad77a81c7a7aad78c1f5
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-01-08 00:40:20 -0500

    Peticiones con ajax

---
+++ Codigo Facilito/JQuery/ajax/peticiones/peticion.php
@@ -0,0 +1,10 @@
+<?
+  $nombre = $_POST['nombre'];
+  $mensaje = $_POST['mensaje'];
+  $email = $_POST['mail'];
+
+  if ($nombre != "" && $mensaje != "" && $email != "")
+    echo 'Correcto';
+  else
+    echo 'Incorrecto';
+?>
