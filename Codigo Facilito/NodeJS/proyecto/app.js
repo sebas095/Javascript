8d809232152a3b0ea0f250e003599eccef2aa307
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-01-16 22:53:51 -0500

    Using Express partials

--- Codigo Facilito/NodeJS/proyecto/app.js
+++ Codigo Facilito/NodeJS/proyecto/app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
+var partials = require('express-partials');
+// var expressLayouts = require('express-ejs-layouts');
 
-app.set("view engine", "jade");
+// app.set("view engine", "jade");
+app.set("view engine", "ejs");
+// app.set("layout", "layout");
+
+app.use(partials());
+// app.use(expressLayouts);
 
 // Gets
 app.get("/", (req, res) => res.render("index"));
