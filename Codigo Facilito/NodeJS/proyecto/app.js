760efc30d189c5441154a040238d71569a194ad4
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-01-26 02:29:56 -0500

    Connect with MongoDB

--- Codigo Facilito/NodeJS/proyecto/app.js
+++ Codigo Facilito/NodeJS/proyecto/app.js
@@ -2,10 +2,22 @@ var express = require('express');
 var bodyParser = require('body-parser'); // Permite extraer parametros del html como form, etc
 var app = express();
 var partials = require('express-partials');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 // var expressLayouts = require('express-ejs-layouts');
 
-//app.set("view engine", "jade");
-app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost/fotos");
+
+var userSchemaJSON = {
+  email: String,
+  password: String
+};
+
+var user_schema = new Schema(userSchemaJSON);
+var User = mongoose.model("User", user_schema);
+
+app.set("view engine", "jade");
+// app.set("view engine", "ejs");
 // app.set("layout", "layout");
 
 app.use(partials());
@@ -16,16 +28,22 @@ app.use(express.static('assets'));
 // app.use(expressLayouts);
 
 // Gets
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => res.render("index1"));
 
 app.get("/login", (req, res) => {
-  res.render("login");
+  User.find((err, doc) => {
+    console.log(doc);
+    res.render("login1");
+  });
 });
 
 app.post("/users", (req, res) => {
-  console.log("Contraseña: " + req.body.password);
-  console.log("Email: " + req.body.email);
-  res.send("Recibimos tus datos.");
+  var user = new User({email: req.body.email, password: req.body.password});
+
+  user.save(() => {
+    res.send("Guardamos tus datos.");
+  });
+
 });
 
 app.listen(8080);
