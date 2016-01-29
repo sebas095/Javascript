85fbd7cde0ffbd1276559c7934bbe4a09b2186b7
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-01-28 20:43:34 -0500

    Add url to the options

---
+++ ReactJS/test/data.js
@@ -0,0 +1,20 @@
+var opt = [
+  {
+    "name": "Home",
+    "values": []
+  },
+  {
+    "name": "Menú",
+    "values": []
+  },
+  {
+    "name": "Problemas",
+    "values": []
+  },
+  {
+   "name": "Contest",
+   "values": [{"name": "Crear Contest", "url": "https://www.youtube.com/watch?v=uaGTjis_ibU"},
+              {"name": "Ver Contest",   "url": "#"},
+              {"name": "More",          "url": "#"}]
+ }
+];
