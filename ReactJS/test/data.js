cc08f5814663f53caed32d3bce5491f43375a5f5
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-01-31 00:51:37 -0500

    Login and Register with bootstrap

--- ReactJS/test/data.js
+++ ReactJS/test/data.js
@@ -1,18 +1,22 @@
 var opt = [
   {
     "name": "Home",
+    "active": true,
     "values": []
   },
   {
     "name": "Menú",
+    "active": false,
     "values": []
   },
   {
     "name": "Problemas",
+    "active": false,
     "values": []
   },
   {
    "name": "Contest",
+   "active": false,
    "values": [{"name": "Crear Contest", "url": "#"},
               {"name": "Ver Contest",   "url": "#"},
               {"name": "More",          "url": "#"}]
