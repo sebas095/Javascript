e0b845151f245f06359bf9916d089b39380a3611
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-01-31 23:58:04 -0500

    Add attributes for forms

--- ReactJS/test/data.js
+++ ReactJS/test/data.js
@@ -1,4 +1,4 @@
-var opt = [
+var nav_bar = [
   {
     "name": "Home",
     "active": true,
@@ -21,4 +21,153 @@ var opt = [
               {"name": "Ver Contest",   "url": "#"},
               {"name": "More",          "url": "#"}]
  }
+];
+
+var form_login = [
+  { // Username
+    "col_label": "3",
+    "col_input": "5",
+    "type": "text",
+    "id": "user",
+    "icon": "user",
+    "name": "Username",
+    "tag": "input",
+    "newline": false,
+    "select": {"value": []}
+  },
+  { // password
+    "col_label": "3",
+    "col_input": "5",
+    "type": "password",
+    "id": "pwd",
+    "icon": "check",
+    "name": "Password",
+    "tag": "input",
+    "newline": false,
+    "select": {"value": []}
+  },
+  { // Remember Me
+    "col_label": "3",
+    "col_input": "5",
+    "type": "checkbox",
+    "id": "",
+    "icon": "",
+    "name": "Remember Me",
+    "tag": "checkbox",
+    "newline": true,
+    "select": {"value": []}
+  },
+  { // Sign In
+    "col_label": "0",
+    "col_input": "5",
+    "type": "button",
+    "id": "",
+    "icon": "",
+    "name": "Sign In",
+    "tag": "btn",
+    "newline": false,
+    "select": {"value": []}
+  }
+];
+
+var form_register = [
+  { // Username
+    "col_label": "",
+    "col_input": "",
+    "type": "",
+    "id": "",
+    "icon": "",
+    "name": "",
+    "tag": "",
+    "newline": false,
+    "select": {"value": []}
+  },
+  { // Full Name
+    "col_label": "",
+    "col_input": "",
+    "type": "",
+    "id": "",
+    "icon": "",
+    "name": "",
+    "tag": "",
+    "newline": false,
+    "select": {"value": []}
+  },
+  { // Password
+    "col_label": "",
+    "col_input": "",
+    "type": "",
+    "id": "",
+    "icon": "",
+    "name": "",
+    "tag": "",
+    "newline": false,
+    "select": {"value": []}
+  },
+  { // Confirm Password
+    "col_label": "",
+    "col_input": "",
+    "type": "",
+    "id": "",
+    "icon": "",
+    "name": "",
+    "tag": "",
+    "newline": false,
+    "select": {"value": []}
+  },
+  { // Email
+    "col_label": "",
+    "col_input": "",
+    "type": "",
+    "id": "",
+    "icon": "",
+    "name": "",
+    "tag": "",
+    "newline": false,
+    "select": {"value": []}
+  },
+  { // Confirm Email
+    "col_label": "",
+    "col_input": "",
+    "type": "",
+    "id": "",
+    "icon": "",
+    "name": "",
+    "tag": "",
+    "newline": false,
+    "select": {"value": []}
+  },
+  { // Country
+    "col_label": "",
+    "col_input": "",
+    "type": "",
+    "id": "",
+    "icon": "",
+    "name": "",
+    "tag": "",
+    "newline": false,
+    "select": {"value": []}
+  },
+  { // Contest, Group, Team Invitation
+    "col_label": "",
+    "col_input": "",
+    "type": "",
+    "id": "",
+    "icon": "",
+    "name": "",
+    "tag": "",
+    "newline": false,
+    "select": {"value": []}
+  },
+  { // Sign Up
+    "col_label": "",
+    "col_input": "",
+    "type": "",
+    "id": "",
+    "icon": "",
+    "name": "",
+    "tag": "",
+    "newline": false,
+    "select": {"value": []}
+  }
 ];
