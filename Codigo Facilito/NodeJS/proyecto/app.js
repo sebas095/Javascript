26242fcc8b1c6b295e50155a3674497a9616d866
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-02-03 01:20:48 -0500

    Validator with Mongoose

--- Codigo Facilito/NodeJS/proyecto/app.js
+++ Codigo Facilito/NodeJS/proyecto/app.js
@@ -30,12 +30,14 @@ app.post("/users", (req, res) => {
   var user = new User({
     email: req.body.email,
     password: req.body.password,
-    password_confirmation: req.body.password_confirmation
+    password_confirmation: req.body.password_confirmation,
+    username: req.body.username
   });
 
   console.log(user.password_confirmation);
 
-  user.save(() => {
+  user.save((err) => {
+    if (err) console.log(String(err));
     res.send("Guardamos tus datos.");
   });
 
--- Codigo Facilito/NodeJS/proyecto/models/user.js
+++ Codigo Facilito/NodeJS/proyecto/models/user.js
@@ -18,14 +18,46 @@ var Schema = mongoose.Schema;
 
 mongoose.connect("mongodb://localhost/fotos");
 
+var posibles_valores = ["M", "F"];
+var email_match = [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Coloca un email válido"];
+var password_validation = {
+  validator: (pwd) => {
+    return this.password_confirmation == pwd;
+  },
+  message: "Las contraseñas no son iguales"
+}
+
 var user_schema = new Schema({
   name: String,
   last_name: String,
-  username: String,
-  password: String,
-  age: Number,
-  email: String,
-  date_of_birth: Date
+  username: {
+    type: String,
+    required: true,
+    maxlength: [50, "Username muy grande"]
+  },
+  password: {
+    type: String,
+    minlength: [8, "El password es muy corto"],
+    validate: password_validation
+  },
+  age: {
+    type: Number,
+    min: [5, "La edad no puede ser menor que 5"],
+    max: [100, "la edad no puede ser mayor que 100"]
+  },
+  email: {
+    type: String,
+    required: "El correo es obligatorio",
+    match: email_match
+  },
+  date_of_birth: Date,
+  sex: {
+    type: String,
+    enum: {
+      values: posibles_valores,
+      message: "Opción no válida"
+    }
+  }
 });
 
 // Atributos virtuales (sirven solo para el objeto y no se guarda en la BD)
