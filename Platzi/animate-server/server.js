9c12f74cd8c88b54b2ac65634dc8f391bdd0abf7
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-02-08 00:30:59 -0500

    Read with fs

--- Platzi/animate-server/server.js
+++ Platzi/animate-server/server.js
@@ -2,13 +2,23 @@
 
 // Requires
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 // Server
 const port = process.env.PORT || 8080;
 const server = http.createServer();
 
 // Callbacks
-var onRequest = (req, res) => res.end("Hola io.js");
+var onRequest = (req, res) => {
+  let fileName = path.join(__dirname, 'public', 'index.html');
+  fs.readFile(fileName, (err, file) => {
+    if (err) return res.end(err.message);
+    res.setHeader('Content-Type', 'text/html');
+    res.end(file);
+  });
+}
+
 var onListening = () => console.log(`Listening on port ${port}`);
 
 // Event Emiter
