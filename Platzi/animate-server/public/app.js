f7840ba69d55e59b1601725a7f0276b37fa0d2c9
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-02-08 01:08:41 -0500

    Add basic routing for a js file and html file

---
+++ Platzi/animate-server/public/app.js
@@ -0,0 +1,6 @@
+const record = document.querySelector('#record');
+
+record.addEventListener('click', function(ev) {
+  ev.preventDefault();
+  console.log('Button Clicked');
+});
--- Platzi/animate-server/server.js
+++ Platzi/animate-server/server.js
@@ -9,23 +9,48 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const server = http.createServer();
 
-// Callbacks
-var onRequest = (req, res) => {
+// Responses
+var serveIndex = (res) => {
   let index = path.join(__dirname, 'public', 'index.html');
 
   // Creamos un RS para leer de manera eficiente el archivo
   // El RS pasa los datos a res cada que tenga algo en el buffer del archivo
   let rs = fs.createReadStream(index);
+  res.setHeader('Conten-Type','text/html');
   rs.pipe(res);
 
   rs.on('error', (err) => {
+    res.setHeader('Conten-Type','text/plain');
     res.end(err.message);
   });
 }
 
+var serveApp = (res) => {
+  let app = path.join(__dirname, 'public', 'app.js');
+  let rs = fs.createReadStream(app);
+
+  res.setHeader('Conten-Type','text/javascript');
+  rs.pipe(res);
+
+  rs.on('error', (err) => {
+    res.setHeader('Conten-Type','text/plain');
+    res.end(err.message);
+  });
+}
+
+// Callbacks
+var onRequest = (req, res) => {
+  let url = req.url;
+  if (url.startsWith('/index') || url === '/') return serveIndex(res);
+  if (url === '/app.js')                       return serveApp(res);
+
+  res.statusCode = 404;
+  res.end(`404 Not Found: ${url}`);
+}
+
 var onListening = () => console.log(`Listening on port ${port}`);
 
-// Event Emiter
+// Event Emiters
 server.on('request', onRequest);
 server.on('listening', onListening);
 
