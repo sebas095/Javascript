bf1d36c1193a59395c2d16550659d1109f30dcb5
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-02-27 00:10:25 -0500

    Add browserify and suport webcam

---
+++ Platzi/animate-server/cliente/app.js
@@ -0,0 +1,23 @@
+const Webrtc2Images = require('webrtc2images');
+
+const rtc = new Webrtc2Images({
+  width: 200,
+  height: 200,
+  frames: 10,
+  type: "image/jpeg",
+  quality: 0.4,
+  interval: 200
+});
+
+rtc.startVideo(function(err) {
+
+});
+
+const record = document.querySelector('#record');
+
+record.addEventListener('click', function(ev) {
+  ev.preventDefault();
+  rtc.recordVideo(function(err, frames) {
+    console.log(frames);
+  });
+}, false);
