c9ee0b7d8079af8077cf944a52bd460358bb4b85
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-02-28 02:01:03 -0500

    coding video in data-uri

--- Platzi/animate-server/helper/video.js
+++ Platzi/animate-server/helper/video.js
@@ -7,6 +7,7 @@ const async = require('async');
 const uuid = require('uuid');
 const dataURIBuffer = require('data-uri-to-buffer');
 const EventEmitter = require('events').EventEmitter;
+const concat = require('concat-stream');
 const listFiles = require('./list');
 const ffmpeg = require('./ffmpeg');
 
@@ -15,12 +16,13 @@ module.exports = function(images) {
   let count = 0
   let baseName = uuid.v4();
   let tmpDir = os.tmpDir();
+  let video;
 
   async.series([
     decodeImages,
     createVideo,
     encodeVideo,
-    //cleanup
+    cleanup
   ], convertFinished);
 
   function decodeImages(done) {
@@ -48,7 +50,15 @@ module.exports = function(images) {
   }
 
   function encodeVideo(done) {
-    done();
+    let fileName = `${baseName}.webm`;
+    let rs = createReadStream(path.join(tmpDir, fileName));
+
+    rs.pipe(concat(function(videoBuffer) {
+      video = `data:video/webm;base64,${videoBuffer.toString('base64')}`;
+      done();
+    }));
+
+    rs.on('error', done);
   }
 
   function cleanup(done) {
@@ -73,9 +83,8 @@ module.exports = function(images) {
   }
 
   function convertFinished(err) {
-    setTimeout(function() {
-      events.emit('video', 'this will be the encoded video');
-    }, 1000);
+    if (err) return events.emit('error', err);
+    events.emit('video', video);
   }
 
   return events;
