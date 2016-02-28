573aeea94b20425865603bcb31021b03a15f8743
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-02-28 00:48:08 -0500

    Decode images and save in the memory

--- Platzi/animate-server/helper/video.js
+++ Platzi/animate-server/helper/video.js
@@ -1,10 +1,18 @@
 'use strict';
 
-const EventEmitter = require('events').EventEmitter;
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
 const async = require('async');
+const uuid = require('uuid');
+const dataURIBuffer = require('data-uri-to-buffer');
+const EventEmitter = require('events').EventEmitter;
 
 module.exports = function(images) {
   let events = new EventEmitter();
+  let count = 0
+  let baseName = uuid.v4();
+  let tmpDir = os.tmpDir();
 
   async.series([
     decodeImages,
@@ -14,7 +22,20 @@ module.exports = function(images) {
   ], convertFinished);
 
   function decodeImages(done) {
-    done();
+    async.eachSeries(images, decodeImage, done);
+  }
+
+  function decodeImage(image, done) {
+    let fileName = `${baseName}-${count++}.jpg`;
+    let buffer = dataURIBuffer(image);
+    let ws = fs.createWriteStream(path.join(tmpDir, fileName));
+
+    // Si hay error ejecuta callback de error
+    // De lo contrario guarar el buffer y pasa al sgte callback
+    ws.on('error', done)
+      .end(buffer, done);
+
+    events.emit('log', `Converting ${fileName}`);
   }
 
   function createVideo(done) {
