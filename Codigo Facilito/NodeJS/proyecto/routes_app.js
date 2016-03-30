48e0eb76213b26a5a8f921851673044e299ef616
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-03-30 01:52:05 -0500

    add basic CRUD

--- Codigo Facilito/NodeJS/proyecto/routes_app.js
+++ Codigo Facilito/NodeJS/proyecto/routes_app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Imagen = require('./models/imagenes');
 
 /* app.com/app/ */
 
@@ -8,5 +9,47 @@ router.get('/', (req, res) => {
 
   res.render("./app/home");
 });
+
+/* REST */
+
+router.get('/imagenes/new', function(req, res) {
+  res.render('app/imagenes/new');
+});
+
+router.get('/imagenes/:id/edit', function(req, res) {
+
+});
+
+router.route("/imagenes/:id")
+  .get(function(req, res) {
+    Imagen.findById(req.params.id, function(err, imagen) {
+      res.render('app/imagenes/show',{imagen: imagen});
+    });
+  })
+  .put(function(req, res) {
+
+  })
+  .delete(function(req, res) {
+
+  });
+
+  router.route("/imagenes")
+    .get(function(req, res) {
+
+    })
+    .post(function(req, res) {
+      var data = {
+        title: req.body.title
+      }
+
+      var imagen = new Imagen(data);
+
+      imagen.save(function(err) {
+        if (!err) {
+          res.redirect('/app/imagenes/' + imagen._id);
+        }
+        else res.render(err);
+      });
+    });
 
 module.exports = router;
